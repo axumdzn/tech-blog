cdbd7fa935822a2629fedc8aa6aba58b41ac941f
Kenneth Felix <kennf03@gmail.com>
2021-10-29 16:58:56 -0400

    added 2 models

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Comment extends Model {};
+
+Class.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+      }
+)
--- server.js
+++ server.js
@@ -1,3 +1,30 @@
-DB_NAME = blog_db
-DB_PASSWORD = password 
-DB_USER = root
+const path = require('path');
+const express = require('express');
+
+const session = require('express-session');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connnection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'something secretive',
+    resave: false,
+    saveUninitialized: true
+};
+
+app.use(session(sess));
+
+//handlebars for later
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.statis(path.join(__dirname,'public')));
+
+app.use(routes);
+
+sequelize.sync({force:false}).then(()=> {
+    app.listen(PORT, ()=> console.log('Now listening'))
+});
