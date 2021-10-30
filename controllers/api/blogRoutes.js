081a0d0c2c7131a64907fe6479dba378e46a07e3
Kenneth Felix <kennf03@gmail.com>
2021-10-30 00:47:07 -0400

    adde dblog route functionaliity

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { AsyncLocalStorage } = require('async_hooks');
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+//create post
+router.post('/',withAuth, async (req,res) => {
+    try{
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newBlog);
+    } catch(err)  {
+        res.status(400).json(err);
+    }
+})
+//grabs current blog
+router.post('/:id',withAuth, async (req,res)=> {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            where: {
+                user_id: req.session.user_id,
+                id: req.params.id
+            }
+        });
+        if(!blogData) {
+            res.status(400).json({message: 'Cannot find blog with this id'})
+        }
+        res.status(200).json(blogData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
