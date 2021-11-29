7e5805023a58d79328ccfa7f889cd66633a83371
Kenneth Felix <kennf03@gmail.com>
2021-11-29 15:13:10 -0500

    signup changes

---
+++ public/js/signup.js
@@ -0,0 +1,33 @@
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const userObj={
+        username:document.querySelector("#username").value,
+        password:document.querySelector("#password").value,
+    };
+    fetch("/api/users",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            fetch("/api/users/login",{
+                method:"POST",
+                body:JSON.stringify(userObj),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }).then(res=>{
+                if(res.ok){
+                   location.href = "/dashboard"
+                } else {
+                    alert("user not found")
+                }
+            })
+        } else {
+            alert("user not created")
+        }
+    });
+});
